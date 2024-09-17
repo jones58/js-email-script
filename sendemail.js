35dd7a1a684c6779386e84df1f7c0dc6f632f4ba
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-17 16:24:57 +0100

    set up postmark email

--- sendemail.js
+++ sendemail.js
@@ -1,10 +1,23 @@
+require("dotenv").config();
 const postmark = require("postmark");
 
 const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
-client.sendEmail({
-  From: process.env.SENDEREMAIL,
-  To: process.env.RECIEVEREMAIL,
-  Subject: "Choose Love Report Submission",
-  TextBody: "New report submitted.",
-});
+function sendEmail() {
+  return client
+    .sendEmail({
+      From: process.env.SENDEREMAIL,
+      To: process.env.RECEIVEREMAIL,
+      Subject: "Choose Love Report Submission",
+      TextBody: "New report submitted.",
+    })
+    .then((response) => {
+      console.log("Email sent successfully", response.messageID);
+    })
+    .catch((error) => {
+      console.error("Email not sent", error.message);
+    });
+}
+
+// Call the function to send the email
+sendEmail();
