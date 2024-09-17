3adbe45c954006014ac10cee45ecf0cdbd2b390d
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-17 16:58:39 +0100

    connecting to airtable and reformatting

--- sendemail.js
+++ sendemail.js
@@ -1,8 +1,35 @@
+// Load environment variables from .env file
 require("dotenv").config();
-const postmark = require("postmark");
 
+// Initialise Postmark client with API key
+const postmark = require("postmark");
 const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
+// Initialise Airtable connection
+const Airtable = require("airtable");
+const base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_BASE_ID);
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+// Store the ID of the last seen record //
+let lastRecordId = "";
+
+// Function to check for new reports in Airtable
+function checkNewReports() {
+  // check for list of reports in Airtable, go over them with a forEach loop
+  table.select({}).eachPage(function page(records) {
+    records.forEach(function (record) {
+      // if the record has not been seen before, send an email
+      if (record.id !== lastRecordId) {
+        sendEmail();
+        lastRecordId = record.id;
+      }
+    });
+  });
+}
+
+// Function to send email notification
 function sendEmail() {
   return client
     .sendEmail({
@@ -19,5 +46,5 @@ function sendEmail() {
     });
 }
 
-// Call the function to send the email
-sendEmail();
+// Initial check when the script starts
+checkNewReports();
